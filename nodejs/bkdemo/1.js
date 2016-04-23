c9ae4046b9a300dae471b83240a86b90d1732ae8
jiaming0709 <742763181@qq.com>
2016-04-23 16:32:11 +0800

    animate layout

---
+++ nodejs/bkdemo/1.js
@@ -0,0 +1,1 @@
+var fs = require("fs");//fs.readFile("data.json","UTF-8",function(error,data){//    if(error){//        console.log(error);//    }//    else{//        console.log(data);//    }//});var data = fs.readFileSync("data.json", "utf-8");console.log(data);console.log("end");
